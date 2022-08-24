065dc2615d3ba636529e87a8c085a37e7f448fe6
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-24 10:00:15 +0200

    :notebook:  Refactoring Code done

---
+++ TodoList/TodoList/ViewModels/TaskViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TaskViewModel.swift
+//  TodoList
+//
+//  Created by IACD-013 on 2022/08/23.
+//
+
+import SwiftUI
+
+class TaskViewModel: ObservableObject{
+        // MARK: Edit Data
+        @Published var editTask: Task?
+}
--- TodoList/TodoList/Views/ContentView.swift
+++ TodoList/TodoList/Views/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @EnvironmentObject var taskModel: TaskViewModel
+    
     @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.editMode) var editButton
     
@@ -22,191 +24,157 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             
-            
             BlobView()
                 .offset(y:350)
                 .blur(radius: 10)
                 .opacity(0.8)
             
-            HStack{
-                VStack(alignment: .leading, spacing: 10){
-                    Text("Today")
-                        .font(.largeTitle.weight(.light))
-                    Spacer()
-                    
-                }
-                .padding()
-                .frame(alignment: .leading)
-                
-                Spacer()
-                VStack(alignment: .trailing, spacing: 5){
-                    
-                        //MARK: - Edit Button
-                    EditButton()
-                        .foregroundColor(.black)
-                    
-                    
-                    Rectangle()
-                        .frame(width: 1, height: 60)
-                    Text("Week")
-                    Rectangle()
-                        .frame(width: 1, height: 10)
-                    Text("Month")
-                    Rectangle()
-                        .frame(width: 1, height: 10)
-                    Text("Year")
-                    Spacer()
-                }
-                .padding()
-                .frame(alignment: .trailing)
-            }
+            TopSectionView()
                 //TODO: - Add Fade when scrolling up
+            
             ScrollView{
-                HStack(alignment: editButton?.wrappedValue == .active ? .center : .top, spacing: 10){
-                    
-                    if editButton?.wrappedValue == .active{
-                        VStack(){
-                            HStack(alignment: .top) {
-                                Text("Title")
-                                    .font(.title)
-                                    .padding(.bottom,10)
-                                
-                                Spacer()
-                                    //MARK: - check buttom
-                                Button {} label: {
-                                    Image(systemName: "checkmark.circle")
-                                        .imageScale(.large)
-                                        .font(.caption)
-                                        .foregroundStyle(.black.opacity(0.8))
-                                }
-                            }
-
-                            VStack {
-                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore aliqua.v")
-                                    .font(.footnote)
-                                    .fontWeight(.light)
-                                    .lineLimit(2)
-                                
-                                Text("Time")
-                                    .font(.caption)
-                                    .padding(.vertical, 2)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                        }
-                        .padding()
-                        .rotationEffect(.degrees(180))
-                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
-                        VStack(spacing: 10){
-                            Button {
-                                    //deleting task
-                                    //Saving delete
-                                
-                                
-                            } label: {
-                                Image(systemName: "doc.fill.badge.plus")
-                                    .font(.title3)
-                                    .foregroundColor(.primary)
-                            }
-                                //MARK: - Delete Button
-                            Button {
-                                    //deleting task
-                                    //Saving delete
-                                
-                                
-                            } label: {
-                                Image(systemName: "minus.circle.fill")
-                                    .font(.title2)
-                                    .foregroundColor(.red)
-                            }
-                                //MARK: - Edit Button
-                            Button {
-                                    //deleting task
-                                    //Saving delete
-                                
-                                
-                            } label: {
-                                Image(systemName: "pencil.circle.fill")
-                                    .font(.title2)
-                                    .foregroundColor(.primary)
-                            }
-                        }
-                    }else{
-                        VStack(){
-                            HStack(alignment: .top) {
-                                Text("Title")
-                                    .font(.title)
-                                    .padding(.bottom,10)
-                                
-                                Spacer()
-                                    //MARK: - check buttom
-                                Button {} label: {
-                                    Image(systemName: "checkmark.circle")
-                                        .imageScale(.large)
-                                        .font(.caption)
-                                        .foregroundStyle(.black.opacity(0.8))
-                                }
-                            }
-                            VStack {
-                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore aliqua.v")
-                                    .font(.footnote)
-                                    .fontWeight(.light)
-                                    .lineLimit(2)
-                                
-                                Text("Time")
-                                    .font(.caption)
-                                    .padding(.vertical, 2)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                        }
-                        .padding()
-                        .rotationEffect(.degrees(180))
-                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
-                            //                        .frame(height: 150)
-                    }
-                }
-                
+               TodayRowCardView
             }
             .offset(y:-120)
             .frame(height: 450)
             .padding()
             .rotationEffect(.degrees(180))
             
-            
                 //MARK: - New Item View
-            VStack {
-                TextField("Title", text: $taskTitle)
-                    .focused($showKeyboardView)
-                    .padding(.horizontal)
-                    .font(.body)
-                    .padding([.horizontal,.top])
-                TextField("Description", text: $taskDescription)
-                    .padding(.horizontal)
-                    .focused($showKeyboardView)
-                    .font(.caption)
-                    .padding()
-                    .toolbar {
-                        ToolbarItemGroup(placement: .keyboard) {
-                            HStack{
-                                Image(systemName: "calendar.badge.plus")
-                                    .imageScale(.large)
-                                Image(systemName: "eyedropper")
-                                    .imageScale(.large)
-                                Spacer()
-                                Image(systemName: "arrow.up.circle.fill")
-                                    .imageScale(.large)
-                                
-                            }
-                            .padding(10)
+            NewItemPopUp
+        }
+        
+    }
+    
+    var NewItemPopUp : some View{
+        VStack {
+            TextField("Title", text: $taskTitle)
+                .focused($showKeyboardView)
+                .padding(.horizontal)
+                .font(.body)
+                .padding([.horizontal,.top])
+            TextField("Description", text: $taskDescription)
+                .padding(.horizontal)
+                .focused($showKeyboardView)
+                .font(.caption)
+                .padding()
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        HStack{
+                            Image(systemName: "calendar.badge.plus")
+                                .imageScale(.large)
+                            Image(systemName: "eyedropper")
+                                .imageScale(.large)
+                            Spacer()
+                            Image(systemName: "arrow.up.circle.fill")
+                                .imageScale(.large)
+                            
+                        }
+                        .padding(10)
+                    }
+                }
+        }
+        .background(.thinMaterial)
+        .cornerRadius(30,corners: [.topLeft,.topRight])
+        .padding(.horizontal,5)
+        .offset(y:showKeyboardView ? 165 : 700)
+        .opacity(showKeyboardView ?  1: 0)
+        .animation(.spring(response: 0.6, dampingFraction: 0.6), value: showKeyboardView)
+    }
+    
+    var TodayRowCardView: some View {
+        HStack(alignment: editButton?.wrappedValue == .active ? .center : .top, spacing: 10){
+            
+            if editButton?.wrappedValue == .active{
+                VStack(){
+                    HStack(alignment: .top) {
+                        Text("Title")
+                            .font(.title)
+                            .padding(.bottom,10)
+                        
+                        Spacer()
+                            //MARK: - check buttom
+                        Button {} label: {
+                            Image(systemName: "checkmark.circle")
+                                .imageScale(.large)
+                                .font(.caption)
+                                .foregroundStyle(.black.opacity(0.8))
                         }
                     }
+                    
+                    VStack {
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore aliqua.v")
+                            .font(.footnote)
+                            .fontWeight(.light)
+                            .lineLimit(2)
+                        
+                        Text("Time")
+                            .font(.caption)
+                            .padding(.vertical, 2)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .padding()
+                .rotationEffect(.degrees(180))
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
+                VStack(spacing: 10){
+                        //MARK: - Archive Button
+                    Button {
+                            //deleting task
+                            //Saving delete
+                    } label: {
+                        Image(systemName: "doc.fill.badge.plus")
+                            .font(.title3)
+                            .foregroundColor(.primary)
+                    }
+                        //MARK: - Delete Button
+                    Button {
+                            //deleting task
+                            //Saving delete
+                        
+                        
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.title2)
+                            .foregroundColor(.red)
+                    }
+
+                }
+            }else{
+                VStack(){
+                    HStack(alignment: .top) {
+                        Text("Title")
+                            .font(.title)
+                            .padding(.bottom,10)
+                        
+                        Spacer()
+                            //MARK: - check buttom
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "checkmark.circle")
+                                .imageScale(.large)
+                                .font(.caption)
+                                .foregroundStyle(.black.opacity(0.8))
+                        }
+                    }
+                    VStack {
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor incididunt ut labore et dolore aliqua.v")
+                            .font(.footnote)
+                            .fontWeight(.light)
+                            .lineLimit(2)
+                        
+                        Text("Time")
+                            .font(.caption)
+                            .padding(.vertical, 2)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .padding()
+                .rotationEffect(.degrees(180))
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
+                    //                        .frame(height: 150)
             }
-            .background(.thinMaterial)
-            .cornerRadius(30,corners: [.topLeft,.topRight])
-            .padding(.horizontal,5)
-            .offset(y:showKeyboardView ? 165 : 700)
-            .opacity(showKeyboardView ?  1: 0)
-            .animation(.spring(response: 0.6, dampingFraction: 0.6), value: showKeyboardView)
-            
         }
     }
 }
@@ -215,7 +183,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            .environmentObject(TaskViewModel())
     }
 }
 
@@ -256,3 +226,40 @@ enum FocusableFeild: Hashable{
     //        }
     //    }
     //}
+
+struct TopSectionView: View {
+    var body: some View {
+        HStack{
+            VStack(alignment: .leading, spacing: 10){
+                Text("Today")
+                    .font(.largeTitle.weight(.light))
+                Spacer()
+                
+            }
+            .padding()
+            .frame(alignment: .leading)
+            
+            Spacer()
+            VStack(alignment: .trailing, spacing: 5){
+                
+                    //MARK: - Edit Button
+                EditButton()
+                    .foregroundColor(.black)
+                Rectangle()
+                    .frame(width: 1, height: 60)
+                Text("Week")
+                Rectangle()
+                    .frame(width: 1, height: 10)
+                Text("Month")
+                Rectangle()
+                    .frame(width: 1, height: 10)
+                Text("Year")
+                Spacer()
+            }
+            .padding()
+            .frame(alignment: .trailing)
+        }
+    }
+}
+
+
